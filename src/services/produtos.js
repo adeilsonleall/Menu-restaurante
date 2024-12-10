44a391c15e64c97b185d2ad6e930c3b06ac1b97c
Adeilson Leal <adeilsonleal.al@gmail.com>
2024-12-10 12:31:18 -0300

    Update produtos.js

--- src/services/produtos.js
+++ src/services/produtos.js
@@ -1,15 +1,16 @@
 import { produtos } from '@/data/data_produtos';
 
 export const filterEntries = () => {
-    return produtos.filter((produto) => produto.categoria === 'Entradas');
+  return produtos.filter((produto) => produto.categoria === 'Entradas');
 }
 
 export const filterProducts = (category) => {
-    return produtos.filter((produto) => produto.categoria === category);
+  return produtos.filter((produto) => produto.categoria === category);
 }
 
 export const shearchProduct = (typedText) => {
-    return produtos.filter((produto) =>
-        produto.nome.toLowerCase().includes(typedText.toLowerCase())
-    )
+  return produtos.filter((produto) =>
+    produto.nome.toLowerCase().includes(typedText.toLowerCase()) ||
+    produto.descricao.toLowerCase().includes(typedText.toLowerCase())
+  )
 }
